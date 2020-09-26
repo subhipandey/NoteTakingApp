de4b06b4026f1e5e04b6a5a249ed48185c024838
Subhi Pandey <subhi2012cpi@gmail.com>
2020-09-26 20:30:59 +0530

    Update TaskDetailsViewModel.kt

--- app/src/main/java/com/subhipandey/android/notetakingapp/viewmodel/TaskDetailsViewModel.kt
+++ app/src/main/java/com/subhipandey/android/notetakingapp/viewmodel/TaskDetailsViewModel.kt
@@ -1,11 +1,32 @@
 package com.subhipandey.android.notetakingapp.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.subhipandey.android.notetakingapp.app.Injection
+import com.subhipandey.android.notetakingapp.data.Task
 import com.subhipandey.android.notetakingapp.data.TaskRepository
+import kotlinx.coroutines.launch
 
 class TaskDetailsViewModel(
 
     private val repository: TaskRepository = Injection.roomRepository
 ) : ViewModel() {
+    val tasks: LiveData<List<Task>?> = repository.getTasks()
+
+    private val _isEmpty = MediatorLiveData<Boolean>()
+    val isEmpty: LiveData<Boolean> = _isEmpty
+
+    init {
+        _isEmpty.addSource(tasks){
+            _isEmpty.postValue(it?.isNullOrEmpty() ?: true)
+        }
+    }
+
+    fun deleteTask(task : Task){
+        viewModelScope.launch{
+            repository.deleteTask(task)
+        }
+    }
 }
