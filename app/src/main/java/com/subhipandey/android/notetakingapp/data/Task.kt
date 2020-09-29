1d33158a1f611460d8274532a3ea8cf5d6f83555
Subhi Pandey <subhi2012cpi@gmail.com>
2020-09-29 20:45:31 +0530

    Update Task.kt

--- app/src/main/java/com/subhipandey/android/notetakingapp/data/Task.kt
+++ app/src/main/java/com/subhipandey/android/notetakingapp/data/Task.kt
@@ -1,14 +1,17 @@
 package com.subhipandey.android.notetakingapp.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 import java.util.UUID
 
 @Entity(tableName = "taskTable")
+@Parcelize
 data class Task(
     val title:String,
     val description: String,
     @PrimaryKey val id: String = UUID.randomUUID().toString()
-) {
+): Parcelable {
     fun isContentEqual(other: Task) = title == other.title && description == other.description
 }
