6a1945a82b7330c0ba8a3999611942263b31b3b2
Subhi Pandey <subhi2012cpi@gmail.com>
2020-09-29 20:44:46 +0530

    Create Extensions.kt

---
+++ app/src/main/java/com/subhipandey/android/notetakingapp/app/Extensions.kt
@@ -0,0 +1,15 @@
+package com.subhipandey.android.notetakingapp.app
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+
+fun Fragment.navigate(directions: NavDirections) = findNavController().navigate(directions)
+
+fun View.hideKeyboard(){
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
