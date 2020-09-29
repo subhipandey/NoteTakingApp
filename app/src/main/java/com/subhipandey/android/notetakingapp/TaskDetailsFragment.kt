5ef030f144b6509071f463432ee005da81fd5f31
Subhi Pandey <subhi2012cpi@gmail.com>
2020-09-29 20:45:26 +0530

    Update TaskDetailsFragment.kt

--- app/src/main/java/com/subhipandey/android/notetakingapp/TaskDetailsFragment.kt
+++ app/src/main/java/com/subhipandey/android/notetakingapp/TaskDetailsFragment.kt
@@ -6,23 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.subhipandey.android.notetakingapp.TaskDetailsFragmentArgs.Companion.fromBundle
+import com.subhipandey.android.notetakingapp.app.hideKeyboard
+import com.subhipandey.android.notetakingapp.data.WorkState
+import com.subhipandey.android.notetakingapp.databinding.FragmentTaskDetailsBinding
+import com.subhipandey.android.notetakingapp.viewmodel.TaskDetailsViewModel
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+
 class TaskDetailsFragment : Fragment() {
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(TaskDetailsViewModel::class.java)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_details, container, false)
-    }
+    ): View? = FragmentTaskDetailsBinding.inflate(inflater, container, false ).also {
+        it.lifecycleOwner = this
+        it.viewModel = this.viewModel
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        arguments?.let { bundle ->
+            viewModel.setTask(fromBundle(bundle).task)
+        }
+
+        viewModel.workState.observe(viewLifecycleOwner, Observer {
+            if (it == WorkState.LOADING){
+                view.hideKeyboard()
+            }
+            if (it == WorkState.FINISH){
+                findNavController().navigateUp()
+            }
+        })
 
 
     }
