e279be379d657dc7558043ca72e42b80c32c74d7
Subhi Pandey <subhi2012cpi@gmail.com>
2020-09-29 17:01:03 +0530

    Update TaskFragment.kt

--- app/src/main/java/com/subhipandey/android/notetakingapp/TaskFragment.kt
+++ app/src/main/java/com/subhipandey/android/notetakingapp/TaskFragment.kt
@@ -6,24 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.subhipandey.android.notetakingapp.data.Task
+import com.subhipandey.android.notetakingapp.databinding.FragmentTasksBinding
+import com.subhipandey.android.notetakingapp.viewmodel.TaskViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class TaskFragment : Fragment() {
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false)
+class TaskFragment : Fragment(), TasksAdapter.TasksListener {
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(TaskViewModel::class.java)
     }
+
+    private val adapter = TasksAdapter(listener = this)
+    private lateinit var binding: FragmentTasksBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = FragmentTasksBinding.inflate(inflater, container, false).also {
+        binding = it
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+
+        })
 
 
+    }
+
+    override fun deleteTaskAtPosition(task: Task, position: Int) {
+
+    }
+
+    override fun onItemClick(task: Task) {
+        viewDetail(task)
+    }
+
+    private fun viewDetail(task: Task? = null) {
+        //  navigate(TasksFragmentDirections.viewDetail(task))
     }
 }
